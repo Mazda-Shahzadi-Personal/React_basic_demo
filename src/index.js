0bf9b11a39f15924d7fc5caa6f933f6995307872
Mazda-Shahzadi-Personal <104691634+Mazda-Shahzadi-Personal@users.noreply.github.com>
2023-01-20 22:30:21 +1100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+//Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello world </h1>
+    <ul>
+      <li>Eggs </li>
+      <li>bacon </li>
+      <li>Toast </li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
